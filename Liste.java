d71300d4797e8a8f253ecb42dd1470fbf7fa5c7c
bartazuk <alexandre.delove@laposte.net>
2014-06-24 12:24:02 +0200

    Update Liste.java
    Ajout de la mise en forme d'url de IETF.

--- Liste.java
+++ Liste.java
@@ -12,12 +12,13 @@ public class Liste {
 		liste= new ArrayList();
 	}
 	
-	public void add(String url) //A utiliser pour mettre le resultat du code qui recupere les urls
+	public void add(String url)
 	{
-		if (!liste.contains(url))//verifie si l'url est pas déjà dans la liste (si plusieurs membres ont publié le même article)
+		String article="https://datatracker.ietf.org"+url;
+		if (!liste.contains(article))//verifie si l'url est pas déjà dans la liste (si plusieurs membres ont publié le même article)
 		{
 			
-			liste.add(url);//ajoute l'url à la liste si il n'y est pas déjà
+			liste.add(article);//ajoute l'url à la liste si il n'y est pas déjà
 		}
 	}
 	public ArrayList<String> getUrl()
